bcd187a781bf6eaf69398d3191a8f32fb2a9c503
himanshumishra-98 <himanshu.pmishra555@gmail.com>
2024-05-07 23:56:26 +0530

    add user routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,4 +1,13 @@
 import { Router } from "express";
+import {upload} from "../middlewares/multer.middleware.js"
+import { 
+  loginUser, 
+  logoutUser, 
+  registerUser, 
+  refreshAccessToken
+} from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
+
 
 const router = Router();
 
@@ -8,7 +17,12 @@ router.route("/register").post(
       name: "avatar",
       maxCount: 1
   }, 
-  ])
+  ]),
+  registerUser
 );
+
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT,  logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
